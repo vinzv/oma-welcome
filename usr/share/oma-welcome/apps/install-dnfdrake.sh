d666d1df23a3d239c72ac9bd9d61a0b7cd2f14ca
rugyada <46781309+rugyada@users.noreply.github.com>
2021-12-24 01:52:56 +0100

    remove packages version

--- usr/share/oma-welcome/apps/install-dnfdrake.sh
+++ usr/share/oma-welcome/apps/install-dnfdrake.sh
@@ -9,7 +9,7 @@ cd /tmp/dnfdrake-download
 dnf install -y dnf-utils python-dnf-plugin-versionlock
 enable-repo unsupported
 kdialog --title $"OpenMandriva" --msgbox $"Work in progress. Please wait... Step 2 of 3."
-dnf download gambas3-gb-dbus-3.15.2-6 gambas3-gb-form-3.15.2-6 gambas3-gb-gtk3-3.15.2-6 gambas3-gb-gui-3.15.2-6 gambas3-gb-image-3.15.2-6 gambas3-gb-qt5-3.15.2-6 gambas3-runtime-3.15.2-6 dnfdrake
+dnf download gambas3-gb-dbus gambas3-gb-form gambas3-gb-gtk3 gambas3-gb-gui gambas3-gb-image gambas3-gb-qt5 gambas3-runtime dnfdrake
 #
 kdialog --title $"OpenMandriva" --msgbox $"Work in progress. Step 3 of 3. Please wait until the process is done, it may take a while"
 rpm -ivh --nodeps *.rpm
