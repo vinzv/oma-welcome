f17ca825080ef11dcc488ce8b18718233191239f
rugyada <46781309+rugyada@users.noreply.github.com>
2022-02-05 18:59:02 +0100

    Add files via upload

--- usr/share/oma-welcome/apps/install-dnfdrake.sh
+++ usr/share/oma-welcome/apps/install-dnfdrake.sh
@@ -2,17 +2,16 @@
 if rpm -q dnfdrake &>/dev/null; then
 	kdialog --title $"OpenMandriva" --msgbox $"DnfDrake is already installed."
 else
-kdialog --title $"OpenMandriva" --msgbox $"Work in progress. Please wait... Step 1 of 3."
-dnf clean all;dnf repolist
-mkdir /tmp/dnfdrake-download
-cd /tmp/dnfdrake-download
-dnf install -y dnf-utils python-dnf-plugin-versionlock
-enable-repo unsupported
-kdialog --title $"OpenMandriva" --msgbox $"Work in progress. Please wait... Step 2 of 3."
-dnf download gambas3-gb-dbus gambas3-gb-form gambas3-gb-gtk3 gambas3-gb-gui gambas3-gb-image gambas3-gb-qt5 gambas3-runtime gambas3-gb-form-stock dnfdrake
-#
-kdialog --title $"OpenMandriva" --msgbox $"Work in progress. Step 3 of 3. Please wait until the process is done, it may take a while"
-rpm -ivh --nodeps *.rpm
+	enable-repo unsupported
+	APP=$"DnfDrake"
+    DB=$(kdialog --title $"OpenMandriva" --progressbar $"Installing $APP" 0)
+	qdbus $DB org.kde.kdialog.ProgressDialog.showCancelButton false
+	pkexec dnf install --assumeyes dnf-utils python-dnf-plugin-versionlock
+	mkdir /tmp/dnfdrake-download
+	cd /tmp/dnfdrake-download
+	sudo dnf download gambas3-gb-dbus gambas3-gb-form gambas3-gb-gtk3 gambas3-gb-gui gambas3-gb-image gambas3-gb-qt5 gambas3-runtime gambas3-gb-form-stock dnfdrake
+	sudo rpm -ivh --nodeps *.rpm
+	qdbus $DB org.kde.kdialog.ProgressDialog.close
 	if rpm -q dnfdrake &>/dev/null; then
 		kdialog --title $"OpenMandriva" --msgbox $"Installation succeeded."
 	else
