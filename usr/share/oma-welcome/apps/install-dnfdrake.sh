4de26a5e6dfc34e44403f86d28b9ca6377d9f605
rugyada <46781309+rugyada@users.noreply.github.com>
2022-03-01 22:58:32 +0100

    Update install-dnfdrake.sh

--- usr/share/oma-welcome/apps/install-dnfdrake.sh
+++ usr/share/oma-welcome/apps/install-dnfdrake.sh
@@ -2,16 +2,14 @@
 if rpm -q dnfdrake &>/dev/null; then
 	kdialog --title $"OpenMandriva" --msgbox $"DnfDrake is already installed."
 else
-kdialog --title $"OpenMandriva" --msgbox $"Work in progress. Please wait... Step 1 of 3"
+kdialog --title $"OpenMandriva" --msgbox $"Work in progress. Please wait until the process is done, it may take a while"
 dnf clean all;dnf repolist
 mkdir /tmp/dnfdrake-download
 cd /tmp/dnfdrake-download
 dnf install -y dnf-utils python-dnf-plugin-versionlock
 enable-repo unsupported
-kdialog --title $"OpenMandriva" --msgbox $"Work in progress. Please wait... Step 2 of 3"
 dnf download gambas3-gb-dbus gambas3-gb-form gambas3-gb-gtk3 gambas3-gb-gui gambas3-gb-image gambas3-gb-qt5 gambas3-runtime gambas3-gb-form-stock dnfdrake
 #
-kdialog --title $"OpenMandriva" --msgbox $"Work in progress. Step 3 of 3. Please wait until the process is done, it may take a while"
 rpm -ivh --nodeps *.rpm
 	if rpm -q dnfdrake &>/dev/null; then
 	kdialog --title $"OpenMandriva" --msgbox $"Installation succeeded."
