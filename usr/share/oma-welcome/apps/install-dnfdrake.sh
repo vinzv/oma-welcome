bee2986b809d0470930af43377de589ee9095d89
rugyada <46781309+rugyada@users.noreply.github.com>
2022-08-12 10:00:12 +0200

    Update install-dnfdrake.sh

--- usr/share/oma-welcome/apps/install-dnfdrake.sh
+++ usr/share/oma-welcome/apps/install-dnfdrake.sh
@@ -6,7 +6,7 @@ kdialog --title $"OpenMandriva" --msgbox $"Installing DnfDrake. Please wait unti
 dnf clean all;dnf repolist
 mkdir /tmp/dnfdrake-download
 cd /tmp/dnfdrake-download
-dnf install -y dnf-utils python-dnf-plugin-versionlock
+dnf install -y dnf-utils python-dnf-plugin-versionlock createrepo_c lsb-release
 enable-repo unsupported
 dnf download gambas3-gb-dbus gambas3-gb-form gambas3-gb-gtk3 gambas3-gb-gui gambas3-gb-image gambas3-gb-qt5 gambas3-runtime gambas3-gb-form-stock dnfdrake
 #
